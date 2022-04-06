3295b18c72ce44df11c3583ee1160fe2ff2f719e
Jamison Burton <jamison@burtontalk.com>
2022-04-06 08:52:38 -0600

    Set up the Form's format

--- CarRental/CarRentalForm.Designer.vb
+++ CarRental/CarRentalForm.Designer.vb
@@ -22,17 +22,438 @@ Partial Class CarRentalForm
     'Do not modify it using the code editor.
     <System.Diagnostics.DebuggerStepThrough()> _
     Private Sub InitializeComponent()
+        Me.TextBox1 = New System.Windows.Forms.TextBox()
+        Me.TextBox2 = New System.Windows.Forms.TextBox()
+        Me.TextBox3 = New System.Windows.Forms.TextBox()
+        Me.TextBox4 = New System.Windows.Forms.TextBox()
+        Me.TextBox5 = New System.Windows.Forms.TextBox()
+        Me.TextBox6 = New System.Windows.Forms.TextBox()
+        Me.TextBox7 = New System.Windows.Forms.TextBox()
+        Me.TextBox8 = New System.Windows.Forms.TextBox()
+        Me.TextBox9 = New System.Windows.Forms.TextBox()
+        Me.TextBox10 = New System.Windows.Forms.TextBox()
+        Me.TextBox11 = New System.Windows.Forms.TextBox()
+        Me.TextBox12 = New System.Windows.Forms.TextBox()
+        Me.TextBox13 = New System.Windows.Forms.TextBox()
+        Me.MilesOrKilometerGroupBox = New System.Windows.Forms.GroupBox()
+        Me.DiscountGroupBox = New System.Windows.Forms.GroupBox()
+        Me.CalculateButton = New System.Windows.Forms.Button()
+        Me.ClearButton = New System.Windows.Forms.Button()
+        Me.SummayButton = New System.Windows.Forms.Button()
+        Me.ExitButton = New System.Windows.Forms.Button()
+        Me.CustomerNameLabel = New System.Windows.Forms.Label()
+        Me.AddressLabel = New System.Windows.Forms.Label()
+        Me.CityLabel = New System.Windows.Forms.Label()
+        Me.StateLabel = New System.Windows.Forms.Label()
+        Me.ZipCodeLabel = New System.Windows.Forms.Label()
+        Me.BeginngOdometerReadindLabel = New System.Windows.Forms.Label()
+        Me.EndingOdometerReadingLabel = New System.Windows.Forms.Label()
+        Me.NumberofDaysLabel = New System.Windows.Forms.Label()
+        Me.YouOweLabel = New System.Windows.Forms.Label()
+        Me.MinusDiscountLabel = New System.Windows.Forms.Label()
+        Me.DayChargeLabel = New System.Windows.Forms.Label()
+        Me.MileageChargeLabel = New System.Windows.Forms.Label()
+        Me.DistanceDrivenInMilesLabel = New System.Windows.Forms.Label()
+        Me.MilesRadioButton = New System.Windows.Forms.RadioButton()
+        Me.KilometerRadioButton = New System.Windows.Forms.RadioButton()
+        Me.AAARadioButton = New System.Windows.Forms.RadioButton()
+        Me.SeniorRadioButton = New System.Windows.Forms.RadioButton()
+        Me.MilesOrKilometerGroupBox.SuspendLayout()
+        Me.DiscountGroupBox.SuspendLayout()
         Me.SuspendLayout()
+        '
+        'TextBox1
+        '
+        Me.TextBox1.Location = New System.Drawing.Point(235, 12)
+        Me.TextBox1.Name = "TextBox1"
+        Me.TextBox1.Size = New System.Drawing.Size(160, 26)
+        Me.TextBox1.TabIndex = 0
+        '
+        'TextBox2
+        '
+        Me.TextBox2.Location = New System.Drawing.Point(235, 44)
+        Me.TextBox2.Name = "TextBox2"
+        Me.TextBox2.Size = New System.Drawing.Size(160, 26)
+        Me.TextBox2.TabIndex = 1
+        '
+        'TextBox3
+        '
+        Me.TextBox3.Location = New System.Drawing.Point(235, 76)
+        Me.TextBox3.Name = "TextBox3"
+        Me.TextBox3.Size = New System.Drawing.Size(160, 26)
+        Me.TextBox3.TabIndex = 2
+        '
+        'TextBox4
+        '
+        Me.TextBox4.Location = New System.Drawing.Point(235, 108)
+        Me.TextBox4.Name = "TextBox4"
+        Me.TextBox4.Size = New System.Drawing.Size(160, 26)
+        Me.TextBox4.TabIndex = 3
+        '
+        'TextBox5
+        '
+        Me.TextBox5.Location = New System.Drawing.Point(235, 236)
+        Me.TextBox5.Name = "TextBox5"
+        Me.TextBox5.Size = New System.Drawing.Size(160, 26)
+        Me.TextBox5.TabIndex = 7
+        '
+        'TextBox6
+        '
+        Me.TextBox6.Location = New System.Drawing.Point(235, 204)
+        Me.TextBox6.Name = "TextBox6"
+        Me.TextBox6.Size = New System.Drawing.Size(160, 26)
+        Me.TextBox6.TabIndex = 6
+        '
+        'TextBox7
+        '
+        Me.TextBox7.Location = New System.Drawing.Point(235, 172)
+        Me.TextBox7.Name = "TextBox7"
+        Me.TextBox7.Size = New System.Drawing.Size(160, 26)
+        Me.TextBox7.TabIndex = 5
+        '
+        'TextBox8
+        '
+        Me.TextBox8.Location = New System.Drawing.Point(235, 140)
+        Me.TextBox8.Name = "TextBox8"
+        Me.TextBox8.Size = New System.Drawing.Size(160, 26)
+        Me.TextBox8.TabIndex = 4
+        '
+        'TextBox9
+        '
+        Me.TextBox9.Location = New System.Drawing.Point(623, 140)
+        Me.TextBox9.Name = "TextBox9"
+        Me.TextBox9.Size = New System.Drawing.Size(132, 26)
+        Me.TextBox9.TabIndex = 12
+        '
+        'TextBox10
+        '
+        Me.TextBox10.Location = New System.Drawing.Point(623, 108)
+        Me.TextBox10.Name = "TextBox10"
+        Me.TextBox10.Size = New System.Drawing.Size(132, 26)
+        Me.TextBox10.TabIndex = 11
+        '
+        'TextBox11
+        '
+        Me.TextBox11.Location = New System.Drawing.Point(623, 76)
+        Me.TextBox11.Name = "TextBox11"
+        Me.TextBox11.Size = New System.Drawing.Size(132, 26)
+        Me.TextBox11.TabIndex = 10
+        '
+        'TextBox12
+        '
+        Me.TextBox12.Location = New System.Drawing.Point(623, 44)
+        Me.TextBox12.Name = "TextBox12"
+        Me.TextBox12.Size = New System.Drawing.Size(132, 26)
+        Me.TextBox12.TabIndex = 9
+        '
+        'TextBox13
+        '
+        Me.TextBox13.Location = New System.Drawing.Point(623, 12)
+        Me.TextBox13.Name = "TextBox13"
+        Me.TextBox13.Size = New System.Drawing.Size(132, 26)
+        Me.TextBox13.TabIndex = 8
+        '
+        'MilesOrKilometerGroupBox
+        '
+        Me.MilesOrKilometerGroupBox.Controls.Add(Me.KilometerRadioButton)
+        Me.MilesOrKilometerGroupBox.Controls.Add(Me.MilesRadioButton)
+        Me.MilesOrKilometerGroupBox.Location = New System.Drawing.Point(12, 286)
+        Me.MilesOrKilometerGroupBox.Name = "MilesOrKilometerGroupBox"
+        Me.MilesOrKilometerGroupBox.Size = New System.Drawing.Size(372, 91)
+        Me.MilesOrKilometerGroupBox.TabIndex = 13
+        Me.MilesOrKilometerGroupBox.TabStop = False
+        Me.MilesOrKilometerGroupBox.Text = "Is the Odometer in Miles or Kilometers"
+        '
+        'DiscountGroupBox
+        '
+        Me.DiscountGroupBox.Controls.Add(Me.SeniorRadioButton)
+        Me.DiscountGroupBox.Controls.Add(Me.AAARadioButton)
+        Me.DiscountGroupBox.Location = New System.Drawing.Point(390, 286)
+        Me.DiscountGroupBox.Name = "DiscountGroupBox"
+        Me.DiscountGroupBox.Size = New System.Drawing.Size(398, 91)
+        Me.DiscountGroupBox.TabIndex = 14
+        Me.DiscountGroupBox.TabStop = False
+        Me.DiscountGroupBox.Text = "Enter any Discounts"
+        '
+        'CalculateButton
+        '
+        Me.CalculateButton.Location = New System.Drawing.Point(122, 396)
+        Me.CalculateButton.Name = "CalculateButton"
+        Me.CalculateButton.Size = New System.Drawing.Size(128, 42)
+        Me.CalculateButton.TabIndex = 15
+        Me.CalculateButton.Text = "Calculate"
+        Me.CalculateButton.UseVisualStyleBackColor = True
+        '
+        'ClearButton
+        '
+        Me.ClearButton.Location = New System.Drawing.Point(256, 396)
+        Me.ClearButton.Name = "ClearButton"
+        Me.ClearButton.Size = New System.Drawing.Size(128, 42)
+        Me.ClearButton.TabIndex = 16
+        Me.ClearButton.Text = "Clear"
+        Me.ClearButton.UseVisualStyleBackColor = True
+        '
+        'SummayButton
+        '
+        Me.SummayButton.Location = New System.Drawing.Point(390, 396)
+        Me.SummayButton.Name = "SummayButton"
+        Me.SummayButton.Size = New System.Drawing.Size(128, 42)
+        Me.SummayButton.TabIndex = 18
+        Me.SummayButton.Text = "Summary"
+        Me.SummayButton.UseVisualStyleBackColor = True
+        '
+        'ExitButton
+        '
+        Me.ExitButton.Location = New System.Drawing.Point(524, 396)
+        Me.ExitButton.Name = "ExitButton"
+        Me.ExitButton.Size = New System.Drawing.Size(128, 42)
+        Me.ExitButton.TabIndex = 17
+        Me.ExitButton.Text = "Exit"
+        Me.ExitButton.UseVisualStyleBackColor = True
+        '
+        'CustomerNameLabel
+        '
+        Me.CustomerNameLabel.AutoSize = True
+        Me.CustomerNameLabel.Location = New System.Drawing.Point(12, 15)
+        Me.CustomerNameLabel.Name = "CustomerNameLabel"
+        Me.CustomerNameLabel.Size = New System.Drawing.Size(124, 20)
+        Me.CustomerNameLabel.TabIndex = 19
+        Me.CustomerNameLabel.Text = "Customer Name"
+        '
+        'AddressLabel
+        '
+        Me.AddressLabel.AutoSize = True
+        Me.AddressLabel.Location = New System.Drawing.Point(12, 47)
+        Me.AddressLabel.Name = "AddressLabel"
+        Me.AddressLabel.Size = New System.Drawing.Size(68, 20)
+        Me.AddressLabel.TabIndex = 20
+        Me.AddressLabel.Text = "Address"
+        '
+        'CityLabel
+        '
+        Me.CityLabel.AutoSize = True
+        Me.CityLabel.Location = New System.Drawing.Point(12, 79)
+        Me.CityLabel.Name = "CityLabel"
+        Me.CityLabel.Size = New System.Drawing.Size(35, 20)
+        Me.CityLabel.TabIndex = 21
+        Me.CityLabel.Text = "City"
+        '
+        'StateLabel
+        '
+        Me.StateLabel.AutoSize = True
+        Me.StateLabel.Location = New System.Drawing.Point(12, 111)
+        Me.StateLabel.Name = "StateLabel"
+        Me.StateLabel.Size = New System.Drawing.Size(48, 20)
+        Me.StateLabel.TabIndex = 22
+        Me.StateLabel.Text = "State"
+        '
+        'ZipCodeLabel
+        '
+        Me.ZipCodeLabel.AutoSize = True
+        Me.ZipCodeLabel.Location = New System.Drawing.Point(12, 143)
+        Me.ZipCodeLabel.Name = "ZipCodeLabel"
+        Me.ZipCodeLabel.Size = New System.Drawing.Size(73, 20)
+        Me.ZipCodeLabel.TabIndex = 26
+        Me.ZipCodeLabel.Text = "Zip Code"
+        '
+        'BeginngOdometerReadindLabel
+        '
+        Me.BeginngOdometerReadindLabel.AutoSize = True
+        Me.BeginngOdometerReadindLabel.Location = New System.Drawing.Point(12, 175)
+        Me.BeginngOdometerReadindLabel.Name = "BeginngOdometerReadindLabel"
+        Me.BeginngOdometerReadindLabel.Size = New System.Drawing.Size(219, 20)
+        Me.BeginngOdometerReadindLabel.TabIndex = 25
+        Me.BeginngOdometerReadindLabel.Text = "Beginning Odometer Reading"
+        '
+        'EndingOdometerReadingLabel
+        '
+        Me.EndingOdometerReadingLabel.AutoSize = True
+        Me.EndingOdometerReadingLabel.Location = New System.Drawing.Point(12, 207)
+        Me.EndingOdometerReadingLabel.Name = "EndingOdometerReadingLabel"
+        Me.EndingOdometerReadingLabel.Size = New System.Drawing.Size(198, 20)
+        Me.EndingOdometerReadingLabel.TabIndex = 24
+        Me.EndingOdometerReadingLabel.Text = "Ending Odometer Reading"
+        '
+        'NumberofDaysLabel
+        '
+        Me.NumberofDaysLabel.AutoSize = True
+        Me.NumberofDaysLabel.Location = New System.Drawing.Point(12, 239)
+        Me.NumberofDaysLabel.Name = "NumberofDaysLabel"
+        Me.NumberofDaysLabel.Size = New System.Drawing.Size(123, 20)
+        Me.NumberofDaysLabel.TabIndex = 23
+        Me.NumberofDaysLabel.Text = "Number of Days"
+        '
+        'YouOweLabel
+        '
+        Me.YouOweLabel.AutoSize = True
+        Me.YouOweLabel.Location = New System.Drawing.Point(436, 143)
+        Me.YouOweLabel.Name = "YouOweLabel"
+        Me.YouOweLabel.Size = New System.Drawing.Size(74, 20)
+        Me.YouOweLabel.TabIndex = 31
+        Me.YouOweLabel.Text = "You Owe"
+        '
+        'MinusDiscountLabel
+        '
+        Me.MinusDiscountLabel.AutoSize = True
+        Me.MinusDiscountLabel.Location = New System.Drawing.Point(436, 111)
+        Me.MinusDiscountLabel.Name = "MinusDiscountLabel"
+        Me.MinusDiscountLabel.Size = New System.Drawing.Size(118, 20)
+        Me.MinusDiscountLabel.TabIndex = 30
+        Me.MinusDiscountLabel.Text = "Minus Discount"
+        '
+        'DayChargeLabel
+        '
+        Me.DayChargeLabel.AutoSize = True
+        Me.DayChargeLabel.Location = New System.Drawing.Point(436, 79)
+        Me.DayChargeLabel.Name = "DayChargeLabel"
+        Me.DayChargeLabel.Size = New System.Drawing.Size(93, 20)
+        Me.DayChargeLabel.TabIndex = 29
+        Me.DayChargeLabel.Text = "Day Charge"
+        '
+        'MileageChargeLabel
+        '
+        Me.MileageChargeLabel.AutoSize = True
+        Me.MileageChargeLabel.Location = New System.Drawing.Point(436, 47)
+        Me.MileageChargeLabel.Name = "MileageChargeLabel"
+        Me.MileageChargeLabel.Size = New System.Drawing.Size(120, 20)
+        Me.MileageChargeLabel.TabIndex = 28
+        Me.MileageChargeLabel.Text = "Mileage Charge"
+        '
+        'DistanceDrivenInMilesLabel
+        '
+        Me.DistanceDrivenInMilesLabel.AutoSize = True
+        Me.DistanceDrivenInMilesLabel.Location = New System.Drawing.Point(436, 15)
+        Me.DistanceDrivenInMilesLabel.Name = "DistanceDrivenInMilesLabel"
+        Me.DistanceDrivenInMilesLabel.Size = New System.Drawing.Size(177, 20)
+        Me.DistanceDrivenInMilesLabel.TabIndex = 27
+        Me.DistanceDrivenInMilesLabel.Text = "Distance Driven in Miles"
+        '
+        'MilesRadioButton
+        '
+        Me.MilesRadioButton.AutoSize = True
+        Me.MilesRadioButton.Location = New System.Drawing.Point(22, 25)
+        Me.MilesRadioButton.Name = "MilesRadioButton"
+        Me.MilesRadioButton.Size = New System.Drawing.Size(70, 24)
+        Me.MilesRadioButton.TabIndex = 0
+        Me.MilesRadioButton.TabStop = True
+        Me.MilesRadioButton.Text = "Miles"
+        Me.MilesRadioButton.UseVisualStyleBackColor = True
+        '
+        'KilometerRadioButton
+        '
+        Me.KilometerRadioButton.AutoSize = True
+        Me.KilometerRadioButton.Location = New System.Drawing.Point(22, 55)
+        Me.KilometerRadioButton.Name = "KilometerRadioButton"
+        Me.KilometerRadioButton.Size = New System.Drawing.Size(108, 24)
+        Me.KilometerRadioButton.TabIndex = 1
+        Me.KilometerRadioButton.TabStop = True
+        Me.KilometerRadioButton.Text = "Kilometers"
+        Me.KilometerRadioButton.UseVisualStyleBackColor = True
+        '
+        'AAARadioButton
+        '
+        Me.AAARadioButton.AutoSize = True
+        Me.AAARadioButton.Location = New System.Drawing.Point(34, 26)
+        Me.AAARadioButton.Name = "AAARadioButton"
+        Me.AAARadioButton.Size = New System.Drawing.Size(196, 24)
+        Me.AAARadioButton.TabIndex = 0
+        Me.AAARadioButton.TabStop = True
+        Me.AAARadioButton.Text = "AAA Member Discount"
+        Me.AAARadioButton.UseVisualStyleBackColor = True
+        '
+        'SeniorRadioButton
+        '
+        Me.SeniorRadioButton.AutoSize = True
+        Me.SeniorRadioButton.Location = New System.Drawing.Point(34, 55)
+        Me.SeniorRadioButton.Name = "SeniorRadioButton"
+        Me.SeniorRadioButton.Size = New System.Drawing.Size(199, 24)
+        Me.SeniorRadioButton.TabIndex = 1
+        Me.SeniorRadioButton.TabStop = True
+        Me.SeniorRadioButton.Text = "Senior Citizen Discount"
+        Me.SeniorRadioButton.UseVisualStyleBackColor = True
         '
         'CarRentalForm
         '
         Me.AutoScaleDimensions = New System.Drawing.SizeF(9.0!, 20.0!)
         Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
         Me.ClientSize = New System.Drawing.Size(800, 450)
+        Me.Controls.Add(Me.YouOweLabel)
+        Me.Controls.Add(Me.MinusDiscountLabel)
+        Me.Controls.Add(Me.DayChargeLabel)
+        Me.Controls.Add(Me.MileageChargeLabel)
+        Me.Controls.Add(Me.DistanceDrivenInMilesLabel)
+        Me.Controls.Add(Me.ZipCodeLabel)
+        Me.Controls.Add(Me.BeginngOdometerReadindLabel)
+        Me.Controls.Add(Me.EndingOdometerReadingLabel)
+        Me.Controls.Add(Me.NumberofDaysLabel)
+        Me.Controls.Add(Me.StateLabel)
+        Me.Controls.Add(Me.CityLabel)
+        Me.Controls.Add(Me.AddressLabel)
+        Me.Controls.Add(Me.CustomerNameLabel)
+        Me.Controls.Add(Me.SummayButton)
+        Me.Controls.Add(Me.ExitButton)
+        Me.Controls.Add(Me.ClearButton)
+        Me.Controls.Add(Me.CalculateButton)
+        Me.Controls.Add(Me.DiscountGroupBox)
+        Me.Controls.Add(Me.MilesOrKilometerGroupBox)
+        Me.Controls.Add(Me.TextBox9)
+        Me.Controls.Add(Me.TextBox10)
+        Me.Controls.Add(Me.TextBox11)
+        Me.Controls.Add(Me.TextBox12)
+        Me.Controls.Add(Me.TextBox13)
+        Me.Controls.Add(Me.TextBox5)
+        Me.Controls.Add(Me.TextBox6)
+        Me.Controls.Add(Me.TextBox7)
+        Me.Controls.Add(Me.TextBox8)
+        Me.Controls.Add(Me.TextBox4)
+        Me.Controls.Add(Me.TextBox3)
+        Me.Controls.Add(Me.TextBox2)
+        Me.Controls.Add(Me.TextBox1)
         Me.Name = "CarRentalForm"
         Me.Text = "Form1"
+        Me.MilesOrKilometerGroupBox.ResumeLayout(False)
+        Me.MilesOrKilometerGroupBox.PerformLayout()
+        Me.DiscountGroupBox.ResumeLayout(False)
+        Me.DiscountGroupBox.PerformLayout()
         Me.ResumeLayout(False)
+        Me.PerformLayout()
 
     End Sub
 
+    Friend WithEvents TextBox1 As TextBox
+    Friend WithEvents TextBox2 As TextBox
+    Friend WithEvents TextBox3 As TextBox
+    Friend WithEvents TextBox4 As TextBox
+    Friend WithEvents TextBox5 As TextBox
+    Friend WithEvents TextBox6 As TextBox
+    Friend WithEvents TextBox7 As TextBox
+    Friend WithEvents TextBox8 As TextBox
+    Friend WithEvents TextBox9 As TextBox
+    Friend WithEvents TextBox10 As TextBox
+    Friend WithEvents TextBox11 As TextBox
+    Friend WithEvents TextBox12 As TextBox
+    Friend WithEvents TextBox13 As TextBox
+    Friend WithEvents MilesOrKilometerGroupBox As GroupBox
+    Friend WithEvents DiscountGroupBox As GroupBox
+    Friend WithEvents CalculateButton As Button
+    Friend WithEvents ClearButton As Button
+    Friend WithEvents SummayButton As Button
+    Friend WithEvents ExitButton As Button
+    Friend WithEvents CustomerNameLabel As Label
+    Friend WithEvents AddressLabel As Label
+    Friend WithEvents CityLabel As Label
+    Friend WithEvents StateLabel As Label
+    Friend WithEvents ZipCodeLabel As Label
+    Friend WithEvents BeginngOdometerReadindLabel As Label
+    Friend WithEvents EndingOdometerReadingLabel As Label
+    Friend WithEvents NumberofDaysLabel As Label
+    Friend WithEvents YouOweLabel As Label
+    Friend WithEvents MinusDiscountLabel As Label
+    Friend WithEvents DayChargeLabel As Label
+    Friend WithEvents MileageChargeLabel As Label
+    Friend WithEvents DistanceDrivenInMilesLabel As Label
+    Friend WithEvents KilometerRadioButton As RadioButton
+    Friend WithEvents MilesRadioButton As RadioButton
+    Friend WithEvents SeniorRadioButton As RadioButton
+    Friend WithEvents AAARadioButton As RadioButton
 End Class
